208404be0fe61e1fe031e2df644fe06880b5f326
Erkinai <erkinai7175@gmail.com>
2023-01-06 23:53:16 +0300

    Updates

---
+++ precache-manifest.0611c7ea2a342572f4454ad38af84b50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe365d66099337dd4ea192554535227",
+    "url": "/randomcountry_uganda/index.html"
+  },
+  {
+    "revision": "041783b1d0a5b4c9194a",
+    "url": "/randomcountry_uganda/static/css/main.2d18621a.chunk.css"
+  },
+  {
+    "revision": "b75377d708a3af48353b",
+    "url": "/randomcountry_uganda/static/js/2.67d9d84c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/randomcountry_uganda/static/js/2.67d9d84c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "041783b1d0a5b4c9194a",
+    "url": "/randomcountry_uganda/static/js/main.04635e0f.chunk.js"
+  },
+  {
+    "revision": "706c597d457449597416",
+    "url": "/randomcountry_uganda/static/js/runtime-main.a63476cf.js"
+  },
+  {
+    "revision": "3ad6f2451c435f7c46eaa619ad95387c",
+    "url": "/randomcountry_uganda/static/media/Flag_of_Uganda.svg.3ad6f245.png"
+  }
+]);
